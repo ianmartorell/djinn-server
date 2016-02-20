5bfd922bd8490a5b0a1e1e7b3ac315c10ad134f0
Ian Martorell <ianmartorell@gmail.com>
2016-02-20 15:22:56 +0100

    Events model and updates to photos model

---
+++ models/events.js
@@ -0,0 +1,19 @@
+var db = require('../db');
+
+exports.create = function(name, cb) {
+	var collection = db.get().collection('events');
+	var event = {
+		name: name
+	};
+	collection.save(event, cb);
+};
+
+exports.get = function(id, cb) {
+	var collection = db.get().collection('events');
+	collection.findOne({_id: id}, cb);
+};
+
+exports.all = function(cb) {
+	var collection = db.get().collection('events');
+	collection.find({}, cb);
+};
--- models/photos.js
+++ models/photos.js
@@ -1,22 +1,27 @@
-var db = require('../db')
+var db = require('../db');
 
-exports.create = function(title, description, cb) {
+exports.create = function(eventId, title, description, cb) {
 	var collection = db.get().collection('photos');
 	var photo = {
+		eventId: eventId,
 		title: title,
 		description: description,
 		date: new Date().toString()
-	}
-
+	};
 	collection.save(photo, cb);
-}
+};
 
 exports.get = function(id, cb) {
 	var collection = db.get().collection('photos');
 	collection.findOne({_id: id}, cb);
-}
+};
+
+exports.fromEvent = function(eventId, cb) {
+	var collection = db.get().collection('photos');
+	collection.find({eventId: eventId}, cb);
+};
 
 exports.all = function(cb) {
 	var collection = db.get().collection('photos');
 	collection.find({}, cb);
-}
+};
