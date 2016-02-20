0eb1fe5c9cd5d93e08fef3d52e2cc66f4ce7a120
Ian Martorell <ianmartorell@gmail.com>
2016-02-20 02:47:27 +0100

    First commit

---
+++ routes/routes.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+module.exports = function(app) {
+
+	app.get('/',function(req,res){
+		res.end("Djinn");
+	});
+
+	app.post('/upload', function(req, res) {
+		console.log(req.files.image.originalFilename);
+		console.log(req.files.image.path);
+		fs.readFile(req.files.image.path, function (err, data){
+			var dirname = "~/djinn";
+			var newPath = dirname + "/uploads/" + 	req.files.image.originalFilename;
+			fs.writeFile(newPath, data, function (err) {
+				if(err){
+					res.json({'response':"Error"});
+				} else {
+					res.json({'response':"Saved"});
+				}
+			});
+		});
+	});
+
+	app.get('/uploads/:file', function (req, res){
+		file = req.params.file;
+		var dirname = "~/djinn";
+		var img = fs.readFileSync(dirname + "/uploads/" + file);
+		res.writeHead(200, {'Content-Type': 'image/jpg' });
+		res.end(img, 'binary');
+	});
+};
